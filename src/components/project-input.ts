db375efa5f607c4e064bc67a858b90578a41ba49
Krisztian Holub <kholub1989@gmail.com>
2020-09-08 08:52:16 +0300

    fix a problem with namespaces imports

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,4 +1,7 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../state/project.ts" />
 
 namespace App {
   // ProjectInput Class
